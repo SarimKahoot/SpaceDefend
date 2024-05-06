006dc9db9eb334ef4d3ae04b89a913129bce0fa9
SarimKahoot <kahootsarimali@gmail.com>
2024-05-06 09:30:54 -0400

    Day 1 commit
    I set a background and was looking for pictures for a space shuttle.

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hi")
+import pygame
+
+
+size = (600, 680)
+screen = pygame.display.set_mode(size)
+
+
+space_bg = pygame.image.load("spacebg.png")
+
+run = True
+
+while run:
+
+
+    for event in pygame.event.get():  # User did something
+        if event.type == pygame.QUIT:  # If user clicked close
+            run = False
+
+    screen.blit(space_bg, (0, 0))
+
+    pygame.display.update()
+
+
+# Once we have exited the main program loop we can stop the game engine:
+pygame.quit()
+

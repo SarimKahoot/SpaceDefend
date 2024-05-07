a4e2a279eeb7e48066e18b86387180a6409767a5
SarimKahoot <kahootsarimali@gmail.com>
2024-05-07 09:28:42 -0400

    5/7/24
    I got a picture for my space shuttle sprite and I made a class file for it, but im having trouble becuase I keep getting errors everytime I run the code that I dont understand why

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+from spaceship import Ship
 
 size = (600, 680)
 screen = pygame.display.set_mode(size)
@@ -7,17 +7,22 @@ screen = pygame.display.set_mode(size)
 
 space_bg = pygame.image.load("spacebg.png")
 
+s = Ship(300, 680)
 run = True
 
 while run:
-
+    keys = pygame.key.get_pressed()  # checking pressed keys
+    if keys[pygame.K_d]:
+        s.move_direction("right")
+    if keys[pygame.K_a]:
+        s.move_direction("left")
 
     for event in pygame.event.get():  # User did something
         if event.type == pygame.QUIT:  # If user clicked close
             run = False
 
     screen.blit(space_bg, (0, 0))
-
+    screen.blit(s.image, s.rect)
     pygame.display.update()
 
 
---
+++ spaceship.py
@@ -0,0 +1,21 @@
+import pygame
+
+
+class Ship:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load("spaceship.png")
+        self.image_size = self.image.get_size()
+        self.rect = pygame.Rect(self.x, self.image_size[0], self.image_size[1])
+        self.delta = .5
+        self.current_direction = "right"
+
+    def move_direction(self, direction):
+        if direction == "right":
+            self.x = self.x + self.delta
+        if direction == "left":
+            self.current_direction = "left"
+            self.x = self.x - self.delta
+        self.rect = pygame.Rect(self.x, self.y, self.image_size[0], self.image_size[1])
